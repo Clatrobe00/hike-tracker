719444f3f952653afabd3100dfafb908c7391b33
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-17 19:57:03 -0700

    update homepage with form input

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Home.css'
 
 const Home = () => {
+
     return (
-        <div>Hello from Home</div>
+        <div className='wrapper'>
+            <div className='form-wrapper'>
+                <form className='hike-form'>
+                    <h3>Enter a new hike!</h3>
+                    <div className='form-item'>
+                        <label for='trail' className='input-label'>Trail Name</label>
+                        <input type='text' id='trail'></input>
+                    </div>
+                    <div className='form-item'>
+                        <label for='distance' className='input-label'>Distance (Miles)</label>
+                        <input type="number" id="distance" min="1" max="100"></input>
+                    </div>
+                    <div className='form-item'>
+                        <input type="button" id="submit" value='submit'></input>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
