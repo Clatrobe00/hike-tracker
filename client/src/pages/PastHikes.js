6cc4e53ce8b7168b0f42fecc5de29415c8d9c7d7
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-19 14:24:01 -0700

    add useEffect API call to PastHikes

--- client/src/pages/PastHikes.js
+++ client/src/pages/PastHikes.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import API from '../utils/HikeAPI';
 
 const PastHikes = () => {
+
+    useEffect(() => {
+        API.getHikes().then((data) => {
+            console.log('data is ', data);
+        })
+    })
+
     return (
         <div>Hello from Past Hikes</div>
     )
