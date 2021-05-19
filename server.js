90cbf8832093c17a1d8e6db036a65af54ac67b74
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-19 14:23:17 -0700

    update server with new API route

--- server.js
+++ server.js
@@ -19,16 +19,21 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+const HikeRoutes = require("./routes/HikeAPI");
+
+app.use(HikeRoutes);
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // Define Mongoose Connection (local host only right now)
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hikes", {
   useNewUrlParser: true,
+  useUnifiedTopology: true 
 });
 
 const db = mongoose.connection;
