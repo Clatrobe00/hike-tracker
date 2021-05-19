c75a8c29eee0d0b3b4f9265a4e1421b8355b98d6
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-18 19:31:28 -0700

    add state and button functionality to input form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,21 +3,35 @@ import './Home.css'
 
 const Home = () => {
 
+    const [trailInfo, setTrailInfo] = useState({
+        trail: '',
+        distance: ''
+    });
+
+    const handleChange = (e) => {
+        console.log(e.target.value)
+        setTrailInfo({ ...trailInfo, [e.target.id]: e.target.value })
+    }
+
+    const handleSubmit = () => {
+        console.log(trailInfo);
+    }
+
     return (
         <div className='wrapper'>
             <div className='form-wrapper'>
                 <form className='hike-form'>
                     <h3>Enter a new hike!</h3>
                     <div className='form-item'>
-                        <label for='trail' className='input-label'>Trail Name</label>
-                        <input type='text' id='trail'></input>
+                        <label className='input-label'>Trail Name</label>
+                        <input type='text' id='trail' value={trailInfo.trail} onChange={handleChange}></input>
                     </div>
                     <div className='form-item'>
-                        <label for='distance' className='input-label'>Distance (Miles)</label>
-                        <input type="number" id="distance" min="1" max="100"></input>
+                        <label className='input-label'>Distance (Miles)</label>
+                        <input type="number" id="distance" min="1" max="100" value={trailInfo.distance} onChange={handleChange}></input>
                     </div>
                     <div className='form-item'>
-                        <input type="button" id="submit" value='submit'></input>
+                        <input type="button" id="submit" value='submit' onClick={handleSubmit}></input>
                     </div>
                 </form>
             </div>
