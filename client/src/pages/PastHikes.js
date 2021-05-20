8dd687a1642c5589d2bd0feec3768666183f2bce
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-19 21:14:49 -0700

    render stored hikes on PastHikes page

--- client/src/pages/PastHikes.js
+++ client/src/pages/PastHikes.js
@@ -1,16 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import API from '../utils/HikeAPI';
 
 const PastHikes = () => {
 
+    const [hikeList, setHikeList] = useState([])
+
+    const RenderSavedList = hikeList.map((hike, index) => {
+        return (
+            <li key={index}>
+                Trail: {hike.trail}
+                Distance: {hike.distance}
+                Date: {hike.date}
+            </li>
+        );
+    })  
+
     useEffect(() => {
         API.getHikes().then((data) => {
-            console.log('data is ', data);
+            const hikes = data.data.map((hike) => {
+                return {trail: hike.trail, distance: hike.distance, date: hike.date}
+            })
+            setHikeList(hikes);
         })
-    })
+    }, [])
+
+    console.log(hikeList);
 
     return (
-        <div>Hello from Past Hikes</div>
+        <div className='container'>
+            <h3>Past Hikes</h3>
+            <div className='past-hike-container'>
+                <ul className='past-hike-list'>
+                    {hikeList[0] ? RenderSavedList : <li>No hikes yet!</li>}
+                </ul>
+            </div>
+        </div>
     )
 }
 
