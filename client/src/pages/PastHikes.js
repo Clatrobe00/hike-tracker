26747ff8b1484b9630fba1d02c4b84ab508300d2
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-21 13:22:17 -0700

    add total distance hiked and new date format

--- client/src/pages/PastHikes.js
+++ client/src/pages/PastHikes.js
@@ -4,7 +4,8 @@ import './PastHikes.css';
 
 const PastHikes = () => {
 
-    const [hikeList, setHikeList] = useState([])
+    const [hikeList, setHikeList] = useState([]);
+    const [totalDistance, setTotalDistance] = useState(0);
 
     const RenderSavedList = hikeList.map((hike, index) => {
         return (
@@ -14,7 +15,7 @@ const PastHikes = () => {
                     <p>Distance: {hike.distance} Miles</p>
                 </div>
                 <div className='trail-footer'>
-                    <p>Date: {hike.date}</p>
+                    <p>{new Date(hike.date).toDateString()}</p>
                 </div>
             </div>
         );
@@ -24,16 +25,24 @@ const PastHikes = () => {
         API.getHikes().then((data) => {
             const hikes = data.data.map((hike) => {
                 return {trail: hike.trail, distance: hike.distance, date: hike.date}
+            });
+            const distance = hikes.map((hike) => {
+                return hike.distance;
             })
+            const totalDistance = distance.reduce((acc, cur) => {
+                return acc += cur
+            }, 0);
+            setTotalDistance(totalDistance);
             setHikeList(hikes);
         })
     }, [])
 
-    console.log(hikeList);
-
     return (
         <div className='container'>
             <h2 className='hike-header'>Past Hikes</h2>
+            <div className='total-distance-container'>
+                <h3>Total Distance Hiked: {totalDistance} Miles</h3>
+            </div>
             <div className='past-hike-container'>
                 {hikeList[0] ? RenderSavedList : <li>No hikes yet!</li>}
             </div>
